55d005d7fcd1b98106dc97f2bfdf6eb3ffc24b9b
maksimveselev <m473080@gmail.com>
2022-06-13 23:57:22 +0700

    commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-function yolochka1(item_1, item_2){
-
-//     let mass = "Hello!"
-//     console.log(mass, item_1, item_2)
-//     console.log(mass, item_1 + item_2)
-
-// }
+    getWordStucture("max")
